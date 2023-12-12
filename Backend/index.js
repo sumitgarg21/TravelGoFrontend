71c8144f5233c58f4b043b35b813fadaea0925c4
Sumit <gargaj3456@gmail.com>
2023-12-12 22:13:16 +0530

    added start command in backend

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.json());
 require("dotenv").config({ path: __dirname + '/.env' });
 const db_link = process.env.DB_LINK;
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 app.listen(port);
 var cors = require("cors");
 const mongoose = require("mongoose")
