bf3b6598d0ffeb1b6a97abcfbcb5e24bd4518bbc
Sumit <gargaj3456@gmail.com>
2023-12-12 13:57:40 +0530

    citylink updated

--- src/components/Citydata.js
+++ src/components/Citydata.js
@@ -3,121 +3,121 @@ const Citydata = [
     "name": "Delhi",
     "description": "Capital city showcasing a blend of history, culture, and vibrant street markets.",
     "image_url": "https://images.unsplash.com/photo-1667849521361-48f5798734f7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZGVsaGklMjB0b3VyaXNtfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
-    "explore_link": "https://example.com/delhi"
+    "explore_link": "https://www.visitacity.com/en/delhi/activities/all-activities"
   },
   {
     "name": "Mumbai",
     "description": "Cosmopolitan metropolis known for its bustling streets and iconic landmarks.",
     "image_url": "https://images.unsplash.com/photo-1570168007204-dfb528c6958f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG11bWJhaXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60",
-    "explore_link": "https://example.com/mumbai"
+    "explore_link": "https://www.visitacity.com/en/mumbai/activities/all-activities"
   },
   {
     "name": "Agra",
     "description": "Home to the magnificent Taj Mahal, a UNESCO World Heritage site.",
     "image_url": "https://images.unsplash.com/photo-1524613032530-449a5d94c285?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGFncmF8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=700&q=60",
-    "explore_link": "https://example.com/agra"
+    "explore_link": "https://www.visitacity.com/en/agra/activities/all-activities"
   },
   {
     "name": "Jaipur",
     "description": '"Pink City" with majestic palaces, forts, and a rich Rajasthani heritage',
     "image_url": "https://images.unsplash.com/photo-1557690756-62754e561982?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8amFpcHVyfGVufDB8fDB8fHww&auto=format&fit=crop&w=700&q=60",
-    "explore_link": "https://example.com/jaipur"
+    "explore_link": "https://www.visitacity.com/en/jaipur/activities/all-activities"
   },
   {
     "name": "Udaipur",
     "description": "City of lakes, featuring stunning palaces, romantic boat rides, and a serene atmosphere.",
     "image_url": "https://images.unsplash.com/photo-1591264247469-d072a1018915?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dWRhaXB1cnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60",
-    "explore_link": "https://example.com/udaipur"
+    "explore_link": "https://www.visitacity.com/en/udaipur/activities/all-activities"
   },
   {
     "name": "Varanasi",
     "description": "Oldest living city in the world, known for its spiritual significance and the sacred Ganges River.",
     "image_url": "https://images.unsplash.com/photo-1634920540678-df0e179c5243?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHZhcmFuYXNpfGVufDB8fDB8fHww&auto=format&fit=crop&w=700&q=60",
-    "explore_link": "https://example.com/varanasi"
+    "explore_link": "https://www.visitacity.com/en/varanasi/activities/all-activities"
   },
   {
     "name": "Goa",
     "description": "Tropical paradise with beautiful beaches, vibrant nightlife, and Portuguese-influenced architecture.",
     "image_url": "https://images.unsplash.com/photo-1590393275627-0c48482c60e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60",
-    "explore_link": "https://example.com/goa"
+    "explore_link": "https://www.visitacity.com/en/goa/activities/all-activities"
   },
   {
     "name": "Kerala",
     "description": "Beautiful coastal state known for its backwaters, lush greenery, and Ayurvedic treatments.",
     "image_url": "https://images.unsplash.com/photo-1602216056096-3b40cc0c9944?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8a2VyYWxhfGVufDB8fDB8fHww&auto=format&fit=crop&w=700&q=60",
-    "explore_link": "https://example.com/kerala"
+    "explore_link": "https://www.visitacity.com/en/kerala/activities/all-activities"
   },
   {
     "name": "Kolkata",
     "description": "Cultural hub of India, famous for its art, literature, and delicious street food.",
     "image_url": "https://images.unsplash.com/photo-1630494378404-097499a0fea1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGtvbGthdGF8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=700&q=60",
-    "explore_link": "https://example.com/kolkata"
+    "explore_link": "https://www.visitacity.com/en/kolkata/activities/all-activities"
   },
   {
     "name": "Chennai",
     "description": "Coastal city with a thriving film industry, temples, and delicious South Indian cuisine.",
     "image_url": "https://images.unsplash.com/photo-1616843413587-9e3a37f7bbd8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2hlbm5haXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60",
-    "explore_link": "https://example.com/chennai"
+    "explore_link": "https://www.visitacity.com/en/chennai/activities/all-activities"
   },
   {
     "name": "Amritsar",
     "description": "Spiritual center of Sikhism, home to the Golden Temple and vibrant bazaars.",
     "image_url": "https://images.unsplash.com/photo-1587899765642-3f8e3ea67852?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YW1yaXRzYXJ8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=700&q=60",
-    "explore_link": "https://example.com/amritsar"
+    "explore_link": "https://www.visitacity.com/en/amritsar/activities/all-activities"
   },
   {
     "name": "Jaisalmer",
     "description": "Majestic desert city with stunning sandstone architecture and camel safaris.",
     "image_url": "https://images.unsplash.com/photo-1627301517152-11505d049286?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8amFpc2FsbWVyfGVufDB8fDB8fHww&auto=format&fit=crop&w=700&q=60",
-    "explore_link": "https://example.com/jaisalmer"
+    "explore_link": "https://www.visitacity.com/en/jaisalmer/activities/all-activities"
   },
   {
     "name": "Shimla",
     "description": " Hill station in the Himalayas, offering picturesque landscapes and a cool climate.",
     "image_url": "https://images.unsplash.com/photo-1562649846-ab413ca01712?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHNoaW1sYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60",
-    "explore_link": "https://example.com/shimla"
+    "explore_link": "https://www.visitacity.com/en/shimla/activities/all-activities"
   },
   {
     "name": "Mysore",
     "description": "Royal city in Karnataka with grand palaces, vibrant markets, and the famous Mysore Dasara festival.",
     "image_url": "https://images.unsplash.com/photo-1590766940554-634a7ed41450?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bXlzb3JlfGVufDB8fDB8fHww&auto=format&fit=crop&w=700&q=60",
-    "explore_link": "https://example.com/mysore"
+    "explore_link": "https://www.visitacity.com/en/mysore/activities/all-activities"
   },
   {
     "name": "Darjeeling",
     "description": "Picturesque hill station in the Himalayas, renowned for tea gardens and stunning views of Kanchenjunga.",
     "image_url": "https://images.unsplash.com/photo-1544634076-a90160ddf44c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZGFyamVlbGluZ3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60",
-    "explore_link": "https://example.com/darjeeling"
+    "explore_link": "https://www.visitacity.com/en/darjeeling/activities/all-activities"
   },
   {
     "name": "Pondicherry",
     "description": "Former French colony with a blend of Indian and French culture, known for its beaches and spiritual retreats.",
     "image_url": "https://images.unsplash.com/photo-1549959846-65be1b8d0c12?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHBvbmRpY2hlcnJ5fGVufDB8fDB8fHww&auto=format&fit=crop&w=700&q=60",
-    "explore_link": "https://example.com/pondicherry"
+    "explore_link": "https://www.visitacity.com/en/pondicherry/activities/all-activities"
   },
   {
     "name": "Rishikesh",
     "description": "Spiritual hub in India, famous for yoga, meditation, and the holy Ganges River.",
     "image_url": "https://images.unsplash.com/photo-1616824421012-296bb3580556?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHJpc2hpa2VzaHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60",
-    "explore_link": "https://example.com/rishikesh"
+    "explore_link": "https://www.visitacity.com/en/rishikesh/activities/all-activities"
   },
   {
     "name": "Hyderabad",
     "description": "Historic city with enchanting palaces, flavorful cuisine, and the iconic Charminar.",
     "image_url": "https://images.unsplash.com/photo-1621909321963-2276c9660298?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGh5ZGVyYWJhZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60",
-    "explore_link": "https://example.com/hyderabad"
+    "explore_link": "https://www.visitacity.com/en/hyderabad/activities/all-activities"
   },
   {
     "name": "Pushkar",
     "description": " Sacred town known for its annual camel fair and the Brahma Temple.",
     "image_url": "https://images.unsplash.com/photo-1584804932859-3ffe33f0982f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cHVzaGthcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60",
-    "explore_link": "https://example.com/pushkar"
+    "explore_link": "https://www.visitacity.com/en/pushkar/activities/all-activities"
   },
   {
     "name": "Kochi",
     "description": "Coastal city in Kerala, offering a blend of colonial history, backwaters, and spice markets.",
     "image_url": "https://images.unsplash.com/photo-1597735881932-d9664c9bbcea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8a29jaGl8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=700&q=60",
-    "explore_link": "https://example.com/kochi"
+    "explore_link": "https://www.visitacity.com/en/kochi/activities/all-activities"
   }
 ]
 export default Citydata
