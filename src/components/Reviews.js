67b3359d9d06940448342984d9283748baca5396
Sumit <gargaj3456@gmail.com>
2023-12-13 23:46:49 +0530

    responsive review card

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,50 +1,58 @@
-import React from 'react'
-import trust from "./images/trust.png"
-import choose from "./images/choose.png"
-import cheap from "./images/cheap.png"
+import React from 'react';
+import trust from "./images/trust.png";
+import choose from "./images/choose.png";
+import cheap from "./images/cheap.png";
+
 export const Reviews = () => {
     return (
-        <div className="d-flex justify-content-around" style={{ backgroundColor: "#f2f2f2", marginTop: "100px" }}>
-            <div className="card " style={{ backgroundColor: "#f2f2f2", maxWidth: "540px", border: "none" }}>
-                <div className="row g-0">
-                    <div className="col-md-4">
-                        <img src={cheap} className="img-fluid rounded-start" alt="cheap" />
-                    </div>
-                    <div className="col-md-8">
-                        <div className="card-body">
-                            <h5 className="card-title"><b>Budget-Friendly Bliss: Unveiling Affordable Delights</b></h5>
-                            <p className="card-text">Discover a treasure trove of affordable experiences.
-                                From cost-effective travel destinations to savvy budget trips, explore the world of affordable enjoyment.</p>
+        <div className="container">
+            <div className="row justify-content-around" style={{ backgroundColor: "#f2f2f2", marginTop: "100px" }}>
+                <div className="col-md-4 mb-4">
+                    <div className="card" style={{ backgroundColor: "#f2f2f2", border: "none" }}>
+                        <div className="row g-0">
+                            <div className="col-md-4">
+                                <img src={cheap} className="img-fluid rounded-start" alt="cheap" />
+                            </div>
+                            <div className="col-md-8">
+                                <div className="card-body">
+                                    <h5 className="card-title"><b>Budget-Friendly Bliss: Unveiling Affordable Delights</b></h5>
+                                    <p className="card-text">Discover a treasure trove of affordable experiences. From cost-effective travel destinations to savvy budget trips, explore the world of affordable enjoyment.</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="card" style={{ backgroundColor: "#f2f2f2", maxWidth: "540px", border: "none", marginLeft: "70px", marginRight: "70px" }}>
-                <div className="row g-0">
-                    <div className="col-md-4">
-                        <img src={trust} className="img-fluid rounded-start" alt="trust" />
-                    </div>
-                    <div className="col-md-8">
-                        <div className="card-body">
-                            <h5 className="card-title"><b>Making Informed Choices for Secure Travel</b></h5>
-                            <p className="card-text">Prioritize trust and safety when making travel decisions. TravelGo ensure a secure and worry-free travel experience.</p>
+                <div className="col-md-4 mb-4">
+                    <div className="card" style={{ backgroundColor: "#f2f2f2", border: "none" }}>
+                        <div className="row g-0">
+                            <div className="col-md-4">
+                                <img src={trust} className="img-fluid rounded-start" alt="trust" />
+                            </div>
+                            <div className="col-md-8">
+                                <div className="card-body">
+                                    <h5 className="card-title"><b>Making Informed Choices for Secure Travel</b></h5>
+                                    <p className="card-text">Prioritize trust and safety when making travel decisions. TravelGo ensures a secure and worry-free travel experience.</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="card" style={{ backgroundColor: "#f2f2f2", maxWidth: "540px", border: "none" }}>
-                <div className="row g-0">
-                    <div className="col-md-4">
-                        <img src={choose} className="img-fluid rounded-start" alt="choose" />
-                    </div>
-                    <div className="col-md-8">
-                        <div className="card-body">
-                            <h5 className="card-title"><b>The Joy of Variety</b></h5>
-                            <p className="card-text">Embrace the freedom to explore diverse options, making decisions that lead to a fulfilling and vibrant journey.</p>
+                <div className="col-md-4 mb-4">
+                    <div className="card" style={{ backgroundColor: "#f2f2f2", border: "none" }}>
+                        <div className="row g-0">
+                            <div className="col-md-4">
+                                <img src={choose} className="img-fluid rounded-start" alt="choose" />
+                            </div>
+                            <div className="col-md-8">
+                                <div className="card-body">
+                                    <h5 className="card-title"><b>The Joy of Variety</b></h5>
+                                    <p className="card-text">Embrace the freedom to explore diverse options, making decisions that lead to a fulfilling and vibrant journey.</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
