98cc311930c372645381d0fd9298a2210014b98f
Sumit <gargaj3456@gmail.com>
2023-12-14 00:35:11 +0530

    Responsive crousel

--- src/components/Crousel.js
+++ src/components/Crousel.js
@@ -2,7 +2,17 @@ import React from "react";
 import homebg from "./images/homebg.jpg";
 import flight from "./images/flight.jpg";
 import hotel from "./images/hotel.jpg";
+
 export const Crousel = () => {
+  const carouselItemStyle = {
+    height: "500px", // Set a fixed height for all carousel items
+  };
+
+  const imageStyle = {
+    height: "100%", // Ensure the image takes up the full height of the carousel item
+    objectFit: "cover",
+  };
+
   return (
     <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
       <div className="carousel-indicators">
@@ -28,48 +38,29 @@ export const Crousel = () => {
         ></button>
       </div>
       <div className="carousel-inner">
-        <div className="carousel-item active" style={{ height: "450px" }}>
-          <img
-            src={homebg}
-            className="d-block w-100"
-            style={{ height: "100%", objectFit: "cover" }}
-            alt="first"
-          />
-          <div className="carousel-caption mb-4 d-none d-md-block">
-            <h1 className="mb-5 display-1">Adventure is out there</h1>
-            <p className="mb-3 fs-5">
-              Get best deals on your customized trip packages
-            </p>
+        <div className="carousel-item active" style={carouselItemStyle}>
+          <img src={homebg} className="d-block w-100" style={imageStyle} alt="first" />
+          <div className="carousel-caption mb-4 d-md-block text-center">
+            <h1 className="mb-3 display-4">Adventure is out there</h1>
+            <p className="mb-3 fs-5">Get the best deals on your customized trip packages</p>
             <button type="button" className="btn btn-primary px-4 py-2 fs-6">
               Check now
             </button>
           </div>
         </div>
-        <div className="carousel-item" style={{ height: "450px" }}>
-          <img
-            src={flight}
-            className="d-block w-100"
-            style={{ height: "100%", objectFit: "cover" }}
-            alt="second"
-          />
-          <div className="carousel-caption mb-5 d-none d-md-block">
-            <p className="mb-3 fs-5">
-              Upto 70% discount on domestic as well as international flights
-            </p>
+        <div className="carousel-item" style={carouselItemStyle}>
+          <img src={flight} className="d-block w-100" style={imageStyle} alt="second" />
+          <div className="carousel-caption mb-5 d-md-block text-center">
+            <p className="mb-3 fs-5">Up to 70% discount on domestic and international flights</p>
             <button type="button" className="btn btn-primary px-4 py-2 fs-6">
               Check now
             </button>
           </div>
         </div>
-        <div className="carousel-item" style={{ height: "450px" }}>
-          <img
-            src={hotel}
-            className="d-block w-100"
-            style={{ height: "100%", objectFit: "cover" }}
-            alt="third"
-          />
-          <div className="carousel-caption mb-5 d-none d-md-block">
-            <p className="mb-3 fs-5">Book Hotelsrooms at the lowest price</p>
+        <div className="carousel-item" style={carouselItemStyle}>
+          <img src={hotel} className="d-block w-100" style={imageStyle} alt="third" />
+          <div className="carousel-caption mb-5 d-md-block text-center">
+            <p className="mb-3 fs-5">Book hotel rooms at the lowest price</p>
             <button type="button" className="btn btn-primary px-4 py-2 fs-6">
               Book now
             </button>
