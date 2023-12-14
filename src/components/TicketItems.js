7ec52c8bd894f049b66cc4cf65f403b14e36a38a
Sumit <gargaj3456@gmail.com>
2023-12-14 23:30:14 +0530

    responsive TicketItems

--- src/components/TicketItems.js
+++ src/components/TicketItems.js
@@ -1,71 +1,73 @@
-import React from 'react'
-import { BASE_URL } from './BackendUrl'
+import React from 'react';
+import { BASE_URL } from './BackendUrl';
+
 export const TicketItems = (props) => {
-    const { Object, Data } = props
+    const { Object, Data } = props;
+
     const book = async (e) => {
-        e.preventDefault()
+        e.preventDefault();
         const response = await fetch(`${BASE_URL}/ticket/saveticket`, {
-            method: "POST",
+            method: 'POST',
             headers: {
-                "Content-Type": "application/json",
-                "authtoken": localStorage.getItem('token')
+                'Content-Type': 'application/json',
+                authtoken: localStorage.getItem('token'),
             },
             body: JSON.stringify({
-
                 mode: Data,
-                name: Object.name
+                name: Object.name,
             }),
         });
         const json = await response.json();
-        console.log(json.success)
+        console.log(json.success);
         if (json.success) {
-            if (Data === "Train") {
-                return window.open("https://www.irctc.co.in/nget/train-search", '_blank');
-            }
-            else if (Data === "Hotel") {
-                return window.open(`${Object.url}`, '_blank');
+            if (Data === 'Train') {
+                window.open('https://www.irctc.co.in/nget/train-search', '_blank');
+            } else if (Data === 'Hotel') {
+                window.open(`${Object.url}`, '_blank');
             }
         } else {
-            return alert("Invalid credentials");
+            alert('Invalid credentials');
         }
-    }
-    if (Data === "Hotel") {
-        const stars = Array.from({ length: Object.hotel_class }, (_, index) => (
-            <span key={index}>⭐</span>
-        ));
-        return (
-            <div>
-                <div className="container shadow p-3 mb-5 bg-body rounded card w-75">
-                    <div className="card-body">
-                        <h5 className="card-title">{Object.name}</h5>
-                        <p className="card-text">Address - {Object.address} {Object.city}</p>
-                        <p className="card-text">{stars}</p>
-                        <p className="card-text">Description - {Object.hotel_description}</p>
-                        <button onClick={book} target="_blank" rel="noreferrer" className="btn btn-outline-warning">Book now</button>
-                    </div>
+    };
+
+    const stars = Array.from({ length: Object.hotel_class }, (_, index) => (
+        <span key={index}>⭐</span>
+    ));
+
+    return (
+        <div className="container mt-4">
+            <div className={`card ${Data === 'Hotel' ? 'mb-3' : 'w-75 mx-auto'}`}>
+                <div className="card-body">
+                    <h5 className="card-title">{Object.name}</h5>
+                    {Data === 'Hotel' && (
+                        <>
+                            <p className="card-text">
+                                Address - {Object.address} {Object.city}
+                            </p>
+                            <p className="card-text">{stars}</p>
+                            <p className="card-text">Description - {Object.hotel_description}</p>
+                        </>
+                    )}
+                    {Data === 'Train' && (
+                        <>
+                            <p className="card-text">
+                                <strong>Departure:</strong> {Object.train_from}, {Object.data.departTime}
+                            </p>
+                            <p className="card-text">
+                                <strong>Arrival:</strong> {Object.train_to}, {Object.data.arriveTime}
+                            </p>
+                            <p className="card-text">
+                                <strong>Classes:</strong> {Object.data.classes.join(', ')}
+                            </p>
+                        </>
+                    )}
+                    <button onClick={book} className="btn btn-outline-warning">
+                        {Data === 'Hotel' ? 'Book now' : 'Check'}
+                    </button>
                 </div>
             </div>
-        )
-    }
-    else if (Data === "Train") {
-        return (
-            <div>
-                <div className="container shadow p-3 mb-5 bg-body rounded card w-50">
-                    <div className="card-body">
-                        <div className="text-center card-title fs-4"><b>{Object.name}({Object.train_num})</b></div><br></br>
-                        <p className="card-text fs-5">
-                            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                                <div>Departure - {Object.train_from},&nbsp;{Object.data.departTime}</div>
-                                <div>Arrival - {Object.train_to},&nbsp;{Object.data.arriveTime}</div>
-                            </div>
-                        </p>
-                        <p className="container card-text">{Object.data.classes.map((item, index) => (
-                            <span key={index}>{item}&nbsp;&nbsp;</span>
-                        ))}</p>
-                        <button onClick={book} className="btn btn-lg btn-outline-warning">Check</button>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-}
+        </div>
+    );
+};
+
+export default TicketItems;
